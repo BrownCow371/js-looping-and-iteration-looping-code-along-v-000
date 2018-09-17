fb748d9ff412959756a662eb4f14136f3fcad4a5
Jamie Brown <jmbrowncow@gmail.com>
2018-09-17 15:13:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function printBadges(myArray) {
-  for(let i = 0, i < array.length, i++) {
+  for (let i = 0; i < array.length; i++) {
     console.log(`Welcome ${myArray[i]}! You are employee #${i+1}.`);
   }
   return myArray;
